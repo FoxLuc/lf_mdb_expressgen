aaea55e26aa059d484434cd5d86a0beac1832eba
FoxLuc <lucaf8502@gmail.com>
2020-11-10 10:53:04 +0000

    05_3_1

--- Lf-expressgen/ESEMPIO_MONGO/routes/movies.js
+++ Lf-expressgen/ESEMPIO_MONGO/routes/movies.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
+const uri = 'mongodb+srv://ferrari_luca:badenoch88@feratlas.sekgr.mongodb.net/FerAtlas?retryWrites=true&w=majority'
 
 /* GET users listing. */
 router.get('/movie_from_title/:title', function (req, res, next) {
     console.log(req.params); //Leggo i parametri passati all'url
     title = req.params.title;
-    const uri = 'mongodb+srv://ferrari_luca:badenoch88@feratlas.sekgr.mongodb.net/FerAtlas?retryWrites=true&w=majority'
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
@@ -20,5 +20,26 @@ router.get('/movie_from_title/:title', function (req, res, next) {
 
     });
 });
+
+router.get('/tenmovies', function(req, res, next){
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(get10Movies);
+
+  function get10Movies(err){
+    if(err) Console.log("Connessione al db non riuscita");
+    else {
+      const collection = client.db("sample_mflix").collection("movies");
+      collection.find().limit(10).toArray(callBackQuery);
+    }
+  }
+
+  function callBackQuery (err, result) {
+    if (err) console.log(err.message);
+    else res.send(result);
+    client.close();
+  }
+})
+
+
 
 module.exports = router;
